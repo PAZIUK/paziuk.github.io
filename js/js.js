460d5b11f80b2765eb2bb55aa085a2a432254b83
Serhiy Paziuk <71930843+PAZIUK@users.noreply.github.com>
2021-04-03 11:00:37 +0300

    Create js.js

---
+++ js/js.js
@@ -0,0 +1,10 @@
+'use strict'
+
+let li = document.getElementsByClassName("project_box");
+let num1 = li.length;
+let num2 = li.length + 1;
+let allProjects = document.getElementById('allProjects');
+for (let i = 0; i != num2; i++) {
+	allProjects.innerHTML = num1;
+	allProjects.style.color = "red";
+};
