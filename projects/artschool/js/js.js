19efb263edf7fe5bc889b0c68239cba68e454c26
PAZIUK <paz.serhiy@gmail.com>
2021-04-11 10:29:45 +0300

    Update site

---
+++ projects/artschool/js/js.js
@@ -0,0 +1,10 @@
+function toggle() {
+	document.querySelector('.mobile-bar').classList.toggle('active');
+	document.querySelector('.toggleMenu').classList.toggle('active');
+}
+function readNext() {
+	document.querySelector('.read-next').classList.toggle('active');
+}
+function closeReadNext() {
+	document.querySelector('.read-next').classList.toggle('active');
+}
