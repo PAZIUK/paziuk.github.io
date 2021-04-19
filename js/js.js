43a505dc2cbaf428db4c50353aeb87aa18d17951
PAZIUK <paz.serhiy@gmail.com>
2021-04-19 10:44:03 +0300

    Add My Colors settings

--- js/js.js
+++ js/js.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict"
 
 let li = document.getElementsByClassName("project_box");
 let num1 = li.length;
@@ -7,6 +7,15 @@ let allProjects = document.getElementById('allProjects');
 for (let i = 0; i != num2; i++) {
 	allProjects.innerHTML = num1;
 	allProjects.style.color = "red";
+};
+
+let color = document.getElementsByClassName("color_box");
+let color1 = color.length;
+let color2 = color.length + 1;
+let allColors = document.getElementById('allColors');
+for (let i = 0; i != num2; i++) {
+	allColors.innerHTML = num1;
+	allColors.style.color = "red";
 };
 
 function toggle() {
