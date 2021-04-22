339b207260459ea249fd9803059babf6c63feb22
PAZIUK <paz.serhiy@gmail.com>
2021-04-22 12:40:02 +0300

    Update Wrapper settings

--- js/wrapperHeight.js
+++ js/wrapperHeight.js
@@ -1,3 +1,3 @@
-let wrapperHeight = clientHeight - document.querySelector('header');
+let wrapperHeight = document.height - document.querySelector('header');
 let wrapper = document.querySelector('.wrapper');
-wrapper.style.maxHeight = wrapperHeight+'px;'
+console.log(wrapperHeight+'px;');
