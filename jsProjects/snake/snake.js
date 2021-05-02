e8a7f75c0edb031f4362cf4fe70081d23e52f365
PAZIUK <paz.serhiy@gmail.com>
2021-05-02 04:22:01 +0300

    Add some to site

--- jsProjects/snake/snake.js
+++ jsProjects/snake/snake.js
@@ -138,8 +138,8 @@ function drawScore() {
     recordBlock.innerHTML = "Record: " + record;
     if (score >= record) {
       recordBlock.innerHTML = "Record: " + record++;
-      if (--record > localStorage.record) {
-        localStorage.setItem("record", --record);
+      if ((record - 1) > localStorage.record) {
+        localStorage.setItem("record", (record - 1));
       }
     }
   }
