b9a0caa1059446cdb785010c4a86bf124d23e07a
PAZIUK <paz.serhiy@gmail.com>
2021-05-28 21:55:48 +0300

    Add some things

--- jsProjects/numberConverter/js.js
+++ jsProjects/numberConverter/js.js
@@ -5,8 +5,8 @@ let showToIt = document.querySelector("ul.to");
 let nosf = document.querySelector(".nosf");
 let nost = document.querySelector(".nost");
 
-let systemsFromSelect = 0;
-let systemsToSelect = 0;
+let systemsFromSelect = "?";
+let systemsToSelect = "?";
 let systemsFrom = {
 	two: function(){
 		systemsFromSelect = 2;
@@ -53,18 +53,24 @@ let systemsTo = {
 	}
 }
 function showFrom(){
-	if(showFromIt.style.opacity == "1"){
-		showFromIt.style.opacity = "0";
+	if(showFromIt.style.display == "flex"){
+		showFromIt.style.display = "none";
 	} else {
-		showFromIt.style.opacity = "1";
+		let top = document.querySelector("div.from").querySelector("p").offsetHeight;
+		top = top + document.querySelector("#from").offsetHeight;
+		showFromIt.style.display = "flex";
+		showFromIt.style.top = top+"px";
 	}
 	
 };
 function showTo(){
-	if(showToIt.style.opacity == "1"){
-		showToIt.style.opacity = "0";
+	if(showToIt.style.display == "flex"){
+		showToIt.style.display = "none";
 	} else {
-		showToIt.style.opacity = "1";
+		let top = document.querySelector("div.to").querySelector("p").offsetHeight;
+		top = top + document.querySelector("#to").offsetHeight;
+		showToIt.style.display = "flex";
+		showToIt.style.top = top+"px";
 	}
 };
 
@@ -72,7 +78,17 @@ function error(){
 	let error = document.querySelector(".error");
 	error.classList.toggle("active");
 }
-
+function changeNum(){
+	let sTS = systemsToSelect
+	let sFS = systemsFromSelect
+	systemsFromSelect = sTS;
+	systemsToSelect = sFS;
+	nost.innerHTML = systemsToSelect;
+	nosf.innerHTML = systemsFromSelect;
+	document.querySelector("#from").innerHTML = "";
+	document.querySelector("#to").innerHTML = "";
+	document.querySelector("img.change-btn").classList.toggle("active");
+}
 
 
 
@@ -87,13 +103,8 @@ function lets(){
 		error();	
 	} else {
 		let result = "";
-		if(systemsFromSelect == 2 && systemsToSelect == 2){
-			result = from;
-		} else if(systemsFromSelect == 8 && systemsToSelect == 8){
-			result = from;
-		} else if(systemsFromSelect == 10 && systemsToSelect == 10){
-			result = from;
-		} else if(systemsFromSelect == 16 && systemsToSelect == 16){
+		if(systemsFromSelect == 2 && systemsToSelect == 2||systemsFromSelect == 8 && systemsToSelect == 8||
+			systemsFromSelect == 10 && systemsToSelect == 10||systemsFromSelect == 16 && systemsToSelect == 16){
 			result = from;
 		} 
 
@@ -130,7 +141,9 @@ function lets(){
 					result += "6";
 				} else if(maybe=="111"){
 					result += "7";
-				} 
+				} else{
+					result += "";
+				}
 			}
 		} else if(systemsFromSelect == 2 && systemsToSelect == 10){
 			let parts = from.split("");
@@ -195,7 +208,7 @@ function lets(){
 				} else if(maybe=="1110"){
 					result += "E";
 				} else if(maybe=="1111"){
-					result += "f";
+					result += "F";
 				}
 			}
 		} 
@@ -328,7 +341,6 @@ function lets(){
 			}
 		} else if(systemsFromSelect == 10 && systemsToSelect == 8){
 			let parts = from.split("");
-
 			do{
 				result = String(Math.floor(from) % 8) + result;
 				from = Math.floor(from/8);
@@ -336,7 +348,6 @@ function lets(){
 			while(Math.floor(from) > 0)
 		} else if(systemsFromSelect == 10 && systemsToSelect == 16){
 			let parts = from.split("");
-
 			do{	
 				let maybe = Math.floor(from) % 16;
 				if(maybe == 10){
@@ -362,11 +373,157 @@ function lets(){
 
 
 		else if(systemsFromSelect == 16 && systemsToSelect == 2){
-			
+			from = from.toUpperCase();
+			let parts = from.split("");
+			let maybe;
+			for(let i=0;i<parts.length;i++){
+				maybe = parts[i];
+				if(maybe=="0"){
+					result += "0000";
+				} else if(maybe=="1"){
+					result += "0001";
+				} else if(maybe=="2"){
+					result += "0010";
+				} else if(maybe=="3"){
+					result += "0011";
+				} else if(maybe=="4"){
+					result += "0100";
+				} else if(maybe=="5"){
+					result += "0101";
+				} else if(maybe=="6"){
+					result += "0110";
+				} else if(maybe=="7"){
+					result += "0111";
+				} else if(maybe=="8"){
+					result += "1000";
+				} else if(maybe=="9"){
+					result += "1001";
+				} else if(maybe=="A"){
+					result += "1010";
+				} else if(maybe=="B"){
+					result += "1011";
+				} else if(maybe=="C"){
+					result += "1100";
+				} else if(maybe=="D"){
+					result += "1101";
+				} else if(maybe=="E"){
+					result += "1110";
+				} else if(maybe=="F"){
+					result += "1111";
+				} else {
+					result += "";
+				}
+			}
 		} else if(systemsFromSelect == 16 && systemsToSelect == 8){
-			
+			from = from.toUpperCase();
+			let parts = from.split("");
+			let maybe;
+			for(let i=0;i<parts.length;i++){
+				maybe = parts[i];
+				if(maybe=="0"){
+					result += "0000";
+				} else if(maybe=="1"){
+					result += "0001";
+				} else if(maybe=="2"){
+					result += "0010";
+				} else if(maybe=="3"){
+					result += "0011";
+				} else if(maybe=="4"){
+					result += "0100";
+				} else if(maybe=="5"){
+					result += "0101";
+				} else if(maybe=="6"){
+					result += "0110";
+				} else if(maybe=="7"){
+					result += "0111";
+				} else if(maybe=="8"){
+					result += "1000";
+				} else if(maybe=="9"){
+					result += "1001";
+				} else if(maybe=="A"){
+					result += "1010";
+				} else if(maybe=="B"){
+					result += "1011";
+				} else if(maybe=="C"){
+					result += "1100";
+				} else if(maybe=="D"){
+					result += "1101";
+				} else if(maybe=="E"){
+					result += "1110";
+				} else if(maybe=="F"){
+					result += "1111";
+				} else {
+					result += "";
+				}
+			}
+			parts = result;
+			if((parts.length % 3) != 0){
+				if((parts.length % 3) == 2){
+					result = "0"+String(result);
+					parts = result.split("");
+				} else if((parts.length % 3) == 1){
+					result = "00"+String(result);
+					parts = result.split("");
+				}
+			}
+			result = "";
+			for(let i=0,b=0;i<=(parts.length/3);i++){
+				maybe = String(parts[b])+String(parts[b+1])+String(parts[b+2]);
+				b+=3;
+				if(maybe=="000"){
+					result += "0";
+				} else if(maybe=="001"){
+					result += "1";
+				} else if(maybe=="010"){
+					result += "2";
+				} else if(maybe=="011"){
+					result += "3";
+				} else if(maybe=="100"){
+					result += "4";
+				} else if(maybe=="101"){
+					result += "5";
+				} else if(maybe=="110"){
+					result += "6";
+				} else if(maybe=="111"){
+					result += "7";
+				} 
+			}
 		} else if(systemsFromSelect == 16 && systemsToSelect == 10){
-			
+			from = from.toUpperCase();
+			let parts = from.split("");
+			for (let i = 0; i < parts.length; i++) {
+				if(parts[i] == "A"){
+					parts[i] = 10;
+				} else if(parts[i] == "B"){
+					parts[i] = 11;
+				} else if(parts[i] == "C"){
+					parts[i] = 12;
+				} else if(parts[i] == "D"){
+					parts[i] = 13;
+				} else if(parts[i] == "E"){
+					parts[i] = 14;
+				} else if(parts[i] == "F"){
+					parts[i] = 15;
+				}  else {
+					error();
+				}
+			}
+
+			let pl = 0;
+			result = 0;
+
+			for (let i = parts.length; i > 0; --i) {
+				let ind = i-1;
+				result += Math.pow(16,ind)*parts[pl];
+				pl++;
+				if(pl == parts.length){
+					break;
+				}
+				
+			}
+			if(isNaN(result)){
+				result = "";
+			}
 		}
 		document.querySelector("#to").innerHTML = result;
 	}
